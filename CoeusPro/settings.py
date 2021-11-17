240e3885dc5c99d6f12752bfbc364b106efe5a25
SmitDeshmukh <smitdeshmukh58@gmail.com>
2021-11-17 14:16:09 +0530

    FinalVersion

--- CoeusPro/settings.py
+++ CoeusPro/settings.py
@@ -130,10 +130,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
