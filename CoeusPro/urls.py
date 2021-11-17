ce8822b8b1006c7470298a1284ad43423d5938ad
SmitDeshmukh <smitdeshmukh58@gmail.com>
2021-11-17 15:21:46 +0530

    FinalVersion

--- CoeusPro/urls.py
+++ CoeusPro/urls.py
@@ -1,4 +1,4 @@
-#from django.views.static import serve
+from django.views.static import serve
 
 from django.conf.urls import url
 from django.contrib import admin
@@ -20,6 +20,6 @@ urlpatterns = [
                   path('HOD', include('HOD.urls', namespace='HOD')),
                   path('classteacher', include('classteacher.urls', namespace='classteacher')),
                   path('profile/<str:user>', views.belongsto, name='profile'),
-                  #url(r'^media/(?P<path>.*)$', serve, {'document_root':       settings.MEDIA_ROOT}),
-                  #url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'^media/(?P<path>.*)$', serve, {'document_root':       settings.MEDIA_ROOT}),
+                  url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+              ]
